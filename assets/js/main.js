7159383bb8ee011d21677d2d6c509bbf0c48438e
devamauryjunior <jobagpj@gmail.com>
2022-08-21 20:34:54 -0300

    modificacao: buton e arquivo js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,32 @@
-// const a = document.querySelector('.material-symbols-outlined');
-// let b = null;
-// a.addEventListener('click', function() {
-//     a.classList.remove('material-symbols-outlined');
-//     a.textContent = 'close';
-//     a.classList.add('material-symbols-outlined');
-//     b = a;
-// });
-// console.log(b);
-
-// const container1 = document.querySelector('.container-1');
-
+// -------------------------------------------------------------
 const getOpenButtonMenu = document.querySelector('#openMenu');
 const getCloseButtonMenu = document.querySelector('#closeMenu');
 const nav = document.querySelector('.navigation');
+// -------------------------------------------------------------
 
+// definindo botão de fechar o menu como display none
 getCloseButtonMenu.style.display = "none";
-getOpenButtonMenu.addEventListener('click', function(){
+
+// -------------------------------------------------------------
+getOpenButtonMenu.addEventListener('click', ()=>{
     nav.classList.toggle('active');
+    // definindo o sroll do budy como "desativo"
     document.body.style.overflow = "hidden";
+    // definindo botão de abrir o menu como display none
     getOpenButtonMenu.style.display = "none";
+    // definindo botão de fechar o menu como display block
     getCloseButtonMenu.style.display = "block";
 })
+// -------------------------------------------------------------
 
-getCloseButtonMenu.onclick = ()=>{
+// -------------------------------------------------------------
+getCloseButtonMenu.onclick = function() {
     nav.classList.toggle('active');
+    // definindo o sroll do budy para scroll, isto é, volta ao normal
     document.body.style.overflow = "scroll";
+    // definindo botão de fechar o menu como display none
     getCloseButtonMenu.style.display = "none";
+    // definindo botão de abrir o menu como display block
     getOpenButtonMenu.style.display = "block";
 }
+// -------------------------------------------------------------
