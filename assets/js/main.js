bfa6900d5b0fa9599fb27fb1f2570c8325a0c5c8
devamauryjunior <jobagpj@gmail.com>
2022-08-21 17:13:45 -0300

    desktop: header finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// const a = document.querySelector('.material-symbols-outlined');
+// let b = null;
+// a.addEventListener('click', function() {
+//     a.classList.remove('material-symbols-outlined');
+//     a.textContent = 'close';
+//     a.classList.add('material-symbols-outlined');
+//     b = a;
+// });
+// console.log(b);
+
+const container1 = document.querySelector('.container-1');
