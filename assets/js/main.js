8ac028dd4d624cfd502ccdb79979e4daf1eed76f
devamauryjunior <jobagpj@gmail.com>
2022-08-21 19:21:48 -0300

    modificacao: css-header-js-main-html-index

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,23 @@
 // });
 // console.log(b);
 
-const container1 = document.querySelector('.container-1');
+// const container1 = document.querySelector('.container-1');
+
+const getOpenButtonMenu = document.querySelector('#openMenu');
+const getCloseButtonMenu = document.querySelector('#closeMenu');
+const nav = document.querySelector('.navigation');
+
+getCloseButtonMenu.style.display = "none";
+getOpenButtonMenu.addEventListener('click', function(){
+    nav.classList.toggle('active');
+    document.body.style.overflow = "hidden";
+    getOpenButtonMenu.style.display = "none";
+    getCloseButtonMenu.style.display = "block";
+})
+
+getCloseButtonMenu.onclick = ()=>{
+    nav.classList.toggle('active');
+    document.body.style.overflow = "scroll";
+    getCloseButtonMenu.style.display = "none";
+    getOpenButtonMenu.style.display = "block";
+}
